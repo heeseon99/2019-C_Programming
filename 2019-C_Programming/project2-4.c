15b16d46acf2f41fea2728b2acc0b46797a8e092
heeseon99 <gmltjs418@gmail.com>
2024-10-04 03:13:46 +0900

    2장 4번
    - 달력 출력

---
+++ 2019-C_Programming/project2-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("월\t화\t수\t목\t금\t토\t일\n");			//화면에 '월 화 수 목 금 토 일'출력 후 줄바꿈, \t는 tab키 누른 것 처럼 커서 이동, 
+	printf("1\t2\t3\t4\t5\t6\t7\n");				//숫자를 나타냄
+	printf("8\t9\t10\t11\t12\t13\t14\n");
+	printf("15\t16\t17\t18\t19\t20\t21\n");
+	printf("22\t23\t24\t25\t27\t27\t28\n");
+	printf("29\t30\t31\n");
+}
