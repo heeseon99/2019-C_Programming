ae2669d7128c278136309c8b0aea18288303f2b7
heeseon99 <gmltjs418@gmail.com>
2024-11-20 21:58:10 +0900

    4장 11번
    - 화면에 아래 내용 출력하는 프로그램
      ASCII code”, ‘A’, ‘B’, ‘C’
      \t \a \n

---
+++ 2019-C_Programming/project4-11.c
@@ -0,0 +1,14 @@
+/*화면에 
+ASCII code”, ‘A’, ‘B’, ‘C’
+ \t \a \n
+과 같이 출력하는 프로그램 작성*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\"ASCII code\", \'A\', \'B\', \'C\'\n");
+	printf("\\t \\a \\n");
+
+	return 0;
+}
