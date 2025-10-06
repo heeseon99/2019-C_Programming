5fc28f3d4db86b0a6e702ecd64a6c22aee909f97
heeseon99 <gmltjs418@gmail.com>
2025-10-06 15:04:51 +0900

    조합 계산하기 - combination(int n, int r) - n개에서 r개를 선택하는 경우의 수 구함 - 조합 : n개의 서로 구별이 가능한 것에서 순서 없이 r개를 택할 수 있는 방법

---
+++ 2019-C_Programming/combination.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int combination(int n, int r)
+{
+	return ((factorial(n)) / (factorial(r) * factorial(n - r)));
+}
+
+int factorial(int n)
+{
+	int i;
+	long result = 1;
+
+	for (i = 1; i <= n; i++)
+		result *= i;
+
+	return result;
+}
+
+int main(void)
+{
+	int x, y;
+
+	printf("정수를 입력하시오 : ");
+	scanf_s("%d", &x);
+	printf("정수를 입력하시오 : ");
+	scanf_s("%d", &y);
+
+	printf("C(%d, %d) = %d\n", x, y, combination(x, y));
+
+	return 0;
+}                                                                          
