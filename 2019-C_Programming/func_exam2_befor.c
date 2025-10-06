bb5212606bfbfa29fba6ff9ca20aeae6b9d4480c
heeseon99 <gmltjs418@gmail.com>
2025-10-06 15:13:10 +0900

    매개변수, 반환값 없는 함수 실습

---
+++ 2019-C_Programming/func_exam2_befor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void HappyBirthday()
+{
+	printf("생일축하합니다.\n");
+	printf("생일축하합니다.\n");
+	printf("사랑하는 친구의 생일축하 합니다!\n");
+}
+
+int main(void)
+{
+	HappyBirthday();
+	return 0;
+}
