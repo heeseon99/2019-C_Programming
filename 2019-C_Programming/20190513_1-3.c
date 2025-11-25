7467591d710637bf6d6a191ee33ee72b4e634adc
heeseon99 <gmltjs418@gmail.com>
2025-11-26 01:42:39 +0900

    2019.05.13 대학생활세미나 과제 1-3
    - 1-1(두 수의 뺄셈, 곱셈 결과 출력), 1-2(두 수를 나누었을 때 얻는 몫과 나머지 출력)의 처리를 각각 함수로 정의하여 102과 102의 작업을 함께 수행

---
+++ 2019-C_Programming/20190513_1-3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int sub(num1, num2)
+{
+	return num1 - num2;
+}
+int mul(num1, num2)
+{
+	return num1 * num2;
+}
+int quo(num1, num2)
+{
+	return num1 / num2;
+}
+int rest(num1, num2)
+{
+	return num1 % num2;
+}
+
+int main(void)
+{
+	int num1, num2;
+	printf("첫번째 정수를 입력하시오 : ");
+	scanf_s("%d", &num1);
+	printf("두번째 정수를 입력하시오 : ");
+	scanf_s("%d", &num2);
+
+	printf("뺄셈 : %d\n", sub(num1, num2));
+	printf("곲셈 : %d\n", mul(num1, num2));
+	printf("몫 : %d\n", quo(num1, num2));
+	printf("나머지 : %d\n", rest(num1, num2));
+
+	return 0;
+}
